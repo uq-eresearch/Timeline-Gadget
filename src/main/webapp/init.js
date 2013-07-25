5029b0b0b0d513cab32110f4c7a610ea1f5067df
uqsmcnau <simon.mcnaughton@uq.edu.au>
2013-07-24 23:17:40 -0700

    Added Import from JSON Action

--- src/main/webapp/init.js
+++ src/main/webapp/init.js
@@ -1,38 +1,54 @@
+var oThis = this;
+
 Ext.onReady(function(){
-    // The action
-    var action = new Ext.Action({
-        text: 'Action 1',
-        handler: function(){
-            Ext.example.msg('Click','You clicked on "Action 1".');
-        },
-        iconCls: 'blist'
-    });
-
-
     var panel = new Ext.Panel({
         width:800,
         height:595,
 
         tbar: [
-            action, {
-                text: 'Action Menu',
-                menu: [action]
+            {
+                text: 'Import Menu',
+                menu: [
+                    new Ext.Action({
+					    text: 'Import RDF/XML',
+					    handler: function(){
+					    	$('#myInput')[0].onchange = function(result) {													 
+								 var reader = new FileReader();
+								 reader.onload = function(e) {
+									 var data = e.target.result;
+									 console.log("Yoman1");
+									 console.log(data);
+								 }
+								 reader.readAsText(result.target.files[0]);
+					        }
+							$('#myInput').click();
+					    }
+					}),
+					new Ext.Action({
+                    text: 'Import From JSON',
+                    handler: function(){
+                    	$('#myInput')[0].onchange = function(result) {													 
+							 var reader = new FileReader();
+							 reader.onload = function(e) {
+								 var data = jQuery.parseJSON(e.target.result);
+								 
+								 createStoryJS({
+				        			type:		'timeline',
+				        			width:		'798',
+				        			height:		'566',
+				        			source:		data,
+				        			embed_id:	'my-timeline'
+				        		});
+							 }
+							 reader.readAsText(result.target.files[0]);
+				        }
+						$('#myInput').click();
+                    }
+                })]
             }
         ],
 
-        html : "<div id=\"my-timeline\"></div>",
-        listeners : {
-        	afterrender : function(c) {
-        		createStoryJS({
-        			type:		'timeline',
-        			width:		'798',
-        			height:		'566',
-        			source:		'../timeline/example_json.json',
-        			embed_id:	'my-timeline',
-        			debug:		true
-        		});
-        	}
-        },
-        renderTo: Ext.getBody()
+        html : "<div id='my-timeline' style='width: 798px; height: 566px;'></div>",
+        renderTo: Ext.get("timelinearea")
     });
 });
